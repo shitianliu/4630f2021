336c8c8ecb225dd6c7287440904c7861c46cef7c
shitianliu <shitian_liu@student.uml.edu>
2021-11-03 21:07:59 -0400

    WeekReport
    weekly report 1

---
+++ WeeklyReport/app/src/main/java/com/example/assignment4/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.assignment4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView username=(TextView)findViewById(R.id.username)
+                TextView password=(TextView)findViewById(R.id.username)
+                        MaterialButton loginbtn=(MaterialButton)
+                    loginbtn.setOnClickListener(new View.OnClickListener()){
+            
+        }
+
+    }
+}
